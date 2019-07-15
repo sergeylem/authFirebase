66a4d3ea0a0d658cff5bbaa60daa4ee9ef69d519
Sergey Lem <sergey.lem@gmail.com>
2019-07-15 15:42:07 +0600

    updated firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,6 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  // apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  // authDomain: 'crwn-db.firebaseapp.com',
-  // databaseURL: 'https://crwn-db.firebaseio.com',
-  // projectId: 'crwn-db',
-  // storageBucket: 'crwn-db.appspot.com',
-  // messagingSenderId: '850995411664',
-  // appId: '1:850995411664:web:7ddc01d597846f65'
   apiKey: "AIzaSyD0Zs0FD6RAWLcSSlomZRfOXdzIHiy3mQk",
   authDomain: "lesson-9-ced74.firebaseapp.com",
   databaseURL: "https://lesson-9-ced74.firebaseio.com",
@@ -17,7 +10,6 @@ const config = {
   storageBucket: "",
   messagingSenderId: "171380821484",
   appId: "1:171380821484:web:a00df035c8762b44"
-
 };
 
 firebase.initializeApp(config);
